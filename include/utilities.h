f9622d14a501db81b8376015a9dec567bd0f391a
Mattia Mencagli <56358696+mattiamencagli@users.noreply.github.com>
2021-01-29 18:22:13 +0100

    Update utilities.h

--- include/utilities.h
+++ include/utilities.h
@@ -1,3 +1,6 @@
+#ifndef __UTILITIES_H__
+#define __UTILITIES_H__
+
 static int get_a_line(FILE *fp, char *buf);
 
 
@@ -8,3 +11,6 @@ static void azzero(double *d, const int n);
 
 
 static double pbc(double x, const double boxby2);
+
+
+#endif
