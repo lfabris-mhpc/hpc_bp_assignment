73834fe8b04e82355d7c158e8fc77a33db66e2f6
lfabris-mhpc <lfabris@sissa.it>
2021-01-31 19:31:20 +0100

    regression script now has a tolerance of 0.001; also checks the second file

--- tests/regression.sh
+++ tests/regression.sh
@@ -1,20 +1,32 @@
-rm -f *.xyz *.dat *.inp *.rest
+#!/bin/bash
+
+function process() {
+	rm *.dat *.inp *.xyz *.rest 
+	
+	cp examples/${ref}.* .
+	
+	../${exe} < ${ref}.inp
+	head -10 ${ref}.dat | awk '{ printf("%d %.6f %.6f %.6f\n", $1, $2, $3, $4); }' > a.dat
+	head -10 reference/${ref}.dat | awk '{ printf("%d %.6f %.6f %.6f\n", $1, $2, $3, $4); }' > b.dat
+	#cmp a.dat b.dat || exit 1
+	check=$(paste a.dat b.dat | awk 'function abs(x) {return (x<0?-x:x);} {for (i=1; i<5; ++i) {j=i+4; if (abs($i-$j) > 0.01) {printf("failed %.6f vs %.6f\n", $i, $j);} } }')
+	
+	rm -f a.dat b.dat
+	
+	if [ -n "${check}" ]
+	then
+		exit 1
+	fi
+}
 
 exe=ljmd.x
 
 #compare with references
-cp examples/argon_108.* .
-../${exe} < argon_108.inp
-head -10 argon_108.dat | awk '{ printf("%d %.6f %.6f %.6f\n", $1, $2, $3, $4); }' > a.dat
-head -10 reference/argon_108.dat | awk '{ printf("%d %.6f %.6f %.6f\n", $1, $2, $3, $4); }' > b.dat
-cmp a.dat b.dat || exit 1
+ref=argon_108
+process
 
-"
-cp examples/argon_2916.* .
-../${exe} < argon_2916.inp
-head -10 argon_2916.dat | awk '{ printf("%d %.6f %.6f %.6f\n", $1, $2, $3, $4); }'> a.dat
-head -10 reference/argon_2916.dat | awk '{ printf("%d %.6f %.6f %.6f\n", $1, $2, $3, $4); }' > b.dat
-cmp a.dat b.dat || exit 1
-"
+ref=argon_2916
+process
 
-rm -f a.dat b.dat
+
+exit 0
