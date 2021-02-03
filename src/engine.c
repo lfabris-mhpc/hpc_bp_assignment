deb0700a7c52698099b580aaa2cd78d5db7a69ef
otroconi <otroconi@dhcp-129-030.wifi.sissa.it>
2021-02-03 09:57:43 +0100

    split verlet and include verlet_1 and verlet_2 in engine.h

--- src/engine.c
+++ src/engine.c
@@ -60,7 +60,7 @@ void force(mdsys_t *sys)
 }
 
 /* velocity verlet */
-void velverlet(mdsys_t *sys)
+void verlet_1(mdsys_t *sys)
 {
     int i;
 
@@ -73,14 +73,17 @@ void velverlet(mdsys_t *sys)
         sys->ry[i] += sys->dt*sys->vy[i];
         sys->rz[i] += sys->dt*sys->vz[i];
     }
+}
 
-    /* compute forces and potential energy */
-    force(sys);
+void verlet_2(mdsys_t *sys)
+{
+    int i;
 
-    /* second part: propagate velocities by another half step */
+    /* second part: propagate velocities by another half step *
     for (i=0; i<sys->natoms; ++i) {
         sys->vx[i] += 0.5*sys->dt / mvsq2e * sys->fx[i] / sys->mass;
         sys->vy[i] += 0.5*sys->dt / mvsq2e * sys->fy[i] / sys->mass;
         sys->vz[i] += 0.5*sys->dt / mvsq2e * sys->fz[i] / sys->mass;
     }
 }
+
--- tests/test_engine.cpp
+++ tests/test_engine.cpp
@@ -30,3 +30,56 @@ TEST(EngineEkin, ekin)
 	ASSERT_DOUBLE_EQ(sys.ekin, Ek_byhand);
 	
 }
+
+TEST(EngineVerlet_1, verlet_1)
+{
+	int nprint, i, check;
+    	char restfile[BLEN], trajfile[BLEN], ergfile[BLEN], line[BLEN];
+    	mdsys_t sys;
+
+	check=read_input_file(&sys, stdin, line, restfile, trajfile, ergfile, &nprint);
+	assert(check==0);
+	
+	check = mdsys_init(&sys);
+	assert(check==0);
+
+	check = read_restart(&sys, restfile);
+	assert(check==0);
+
+	const double VERLET_1_byhand{0.012246398,0.005674874,0.005674874,0.0024492796,0.00113497486,0.00113497486};
+
+	verlet_1(&sys);
+	ASSERT_DOUBLE_EQ(sys.velverlet_1->rx[0], VERLET_1_byhand[0]);	
+	ASSERT_DOUBLE_EQ(sys.velverlet_1->ry[0], VERLET_1_byhand[1]);
+	ASSERT_DOUBLE_EQ(sys.velverlet_1->rz[0], VERLET_1_byhand[2]);
+
+	ASSERT_DOUBLE_EQ(sys.velverlet_1->vx[0], VERLET_1_byhand[3]);	
+	ASSERT_DOUBLE_EQ(sys.velverlet_1->vy[0], VERLET_1_byhand[4]);	
+	ASSERT_DOUBLE_EQ(sys.velverlet_1->vz[0], VERLET_1_byhand[5]);
+	
+}
+
+TEST(EngineVerlet_2, verlet_2)
+{
+	int nprint, i, check;
+    	char restfile[BLEN], trajfile[BLEN], ergfile[BLEN], line[BLEN];
+    	mdsys_t sys;
+
+	check=read_input_file(&sys, stdin, line, restfile, trajfile, ergfile, &nprint);
+	assert(check==0);
+	
+	check = mdsys_init(&sys);
+	assert(check==0);
+
+	check = read_restart(&sys, restfile);
+	assert(check==0);
+
+	const double VERLET_2_byhand{0.0024492796,0.00113497486,0.00113497486};
+
+	verlet_2(&sys);
+
+	ASSERT_DOUBLE_EQ(sys.verlet_2->vx[0], VERLET_2_byhand[0]);	
+	ASSERT_DOUBLE_EQ(sys.verlet_2->vy[0], VERLET_2_byhand[1]);	
+	ASSERT_DOUBLE_EQ(sys.verlet_2->vz[0], VERLET_2_byhand[2]);
+
+}
